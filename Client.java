adcfe4a16da97a148616f8beaca7d99d7bba2d09
venkateshmantha <manthavenkatesh@gmail.com>
2017-07-11 17:41:45 -0400

    code changes

--- Client.java
+++ Client.java
@@ -16,130 +16,130 @@ import java.net.UnknownHostException;
 
 public class Client implements Runnable {
 
-  private static Socket clientSocket = null;
-  private static ObjectOutputStream os = null;
-  private static ObjectInputStream is = null;
-  private static BufferedReader inputLine = null;
-  private static boolean closed = false;
-  
-  public static void main(String[] args) {
+	private static Socket clientSocket = null;
+	private static ObjectOutputStream os = null;
+	private static ObjectInputStream is = null;
+	private static BufferedReader inputLine = null;
+	private static boolean closed = false;
 
-    // The default port.
-    int portNumber = 2222;
-    // The default host.
-    String host = "localhost";
+	public static void main(String[] args) {
 
-    if (args.length < 2) {
-      System.out.println("Client connected to " + host + " on portNumber " + portNumber);
-    } else {
-      host = args[0];
-      portNumber = Integer.valueOf(args[1]).intValue();
-    }
+		// The default port
+		int portNumber = 2222;
+		// The default host
+		String host = "localhost";
 
-    /*
-     * Open a socket on a given host and port. Open input and output streams.
-     */
-    try {
-	      clientSocket = new Socket(host, portNumber);
-	      inputLine = new BufferedReader(new InputStreamReader(System.in));
-	      os = new ObjectOutputStream(clientSocket.getOutputStream());
-	      is = new ObjectInputStream(clientSocket.getInputStream());
-	    } catch (UnknownHostException e) {
-	      System.err.println("Don't know about host " + host);
-	    } catch (IOException e) {
-	      System.err.println("Couldn't get I/O for the connection to the host "
-	          + host);
-	    }
+		if (args.length < 2) {
+			System.out.println("Client connected to " + host + " on portNumber " + portNumber);
+		} else {
+			host = args[0];
+			portNumber = Integer.valueOf(args[1]).intValue();
+		}
 
-    if (clientSocket != null && os != null && is != null) {
-      try {
-    	  	new Thread(new Client()).start();
-	        while (!closed) {
-	        	System.out.println("Please enter your command in the next line");
-	        	os.writeObject(inputLine.readLine().trim());
-	        }
-	        
-        /*
-         * Close the output stream, the input stream and the socket.
-         */
-	        os.close();
-	        is.close();
-	        clientSocket.close();
-	      } catch (IOException e) {
-	        System.err.println("IOException:  " + e);
-	      }
-    }
-  }
+		/*
+		 * Open the socket connection, input and output streams
+		 */
+		try {
+			clientSocket = new Socket(host, portNumber);
+			inputLine = new BufferedReader(new InputStreamReader(System.in));
+			os = new ObjectOutputStream(clientSocket.getOutputStream());
+			is = new ObjectInputStream(clientSocket.getInputStream());
+		} catch (UnknownHostException e) {
+			System.err.println("Unknown host " + host);
+		} catch (IOException e) {
+			System.err.println("Couldn't get I/O for the connection to the host "
+					+ host);
+		}
 
-  /*
-   * Create a thread to read from the server
-   */
-  
-  public void run() {
-    /*
-     * Keep on reading from the socket until we receive "Bye" from the
-     * server
-     */
-    String responseLine;
-    try {
-      while ((responseLine = (String)is.readObject()) != null) {
-    	  
-    	  if(responseLine.equals("FILE INCOMING")){
-    		  String cname = (String)is.readObject();
-    		  String filename = (String)is.readObject();
-    		  
-    		  int test = is.read();
-    		  	while(test!=-1)
-    		  	{
+		if (clientSocket != null && os != null && is != null) {
+			try {
+				new Thread(new Client()).start();
+				while (!closed) {
+					System.out.println("Please enter your command on the next line");
+					os.writeObject(inputLine.readLine().trim());
+				}
 
-	    		  	int size = 8096;
-	    		  	int byteread;
-	    		  	
-	    		    byte[] buffer = new byte[size];
-	    		    File f = new File(cname + "/" +filename);
-	    		    if(!f.getParentFile().exists()){
-	    		        f.getParentFile().mkdirs();
-	    		    }
-	    		    
-	    		    try {
-	    		        	f.createNewFile();
-	    		        } catch (Exception e) {
-	    		        	e.printStackTrace();
-	    		        }
-	    		    
-	    		    File file = new File(f.getParentFile(), f.getName());
-	    		    System.out.println("The file " + filename + " has been received");
-	    		    try {
-		    		    FileOutputStream fos = new FileOutputStream(file);
-		    	        BufferedOutputStream out = new BufferedOutputStream(fos);
-		    	        while ((byteread = is.read(buffer, 0, buffer.length)) != -1) {
-		    	        	  out.write(buffer, 0, byteread);
-		    	        	  out.flush();
-		    	        	}
-		    	        
-		    	        fos.close();
-		    	        out.close();
-		    	        
-		    	        break;
-	    	        } catch(Exception e) {
-		            	System.out.println("Exception: " +e);
-		            }
-	    		    
-	    		    
-	    	  }
-    	  }
-    	  
-    	  else {
-	        System.out.println(responseLine);
-	        if (responseLine.indexOf("~ Bye") != -1)
-	          break;
-    	  }
-      }
-      closed = true;
-    } catch (IOException e) {
-      System.err.println("IOException:  " + e);
-    } catch (ClassNotFoundException e) {
-		e.printStackTrace();
+				/*
+				 * Close the output stream, input stream and the socket
+				 */
+				os.close();
+				is.close();
+				clientSocket.close();
+			} catch (IOException e) {
+				System.err.println("IOException:  " + e);
+			}
+		}
 	}
-  }
+
+	/*
+	 * Create a thread to read from the server
+	 */
+
+	public void run() {
+		/*
+		 * Keep on reading from the socket until we receive <Bye> from the
+		 * server
+		 */
+		String responseLine;
+		try {
+			while ((responseLine = (String)is.readObject()) != null) {
+
+				if(responseLine.equals("FILE INCOMING")){
+					String cname = (String)is.readObject();
+					String filename = (String)is.readObject();
+
+					int test = is.read();
+					while(test!=-1)
+					{
+
+						int size = 8096;
+						int byteread;
+
+						byte[] buffer = new byte[size];
+						File f = new File(cname + "/" +filename);
+						if(!f.getParentFile().exists()){
+							f.getParentFile().mkdirs();
+						}
+
+						try {
+							f.createNewFile();
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+
+						File file = new File(f.getParentFile(), f.getName());
+						System.out.println("The file " + filename + " has been received");
+						try {
+							FileOutputStream fos = new FileOutputStream(file);
+							BufferedOutputStream out = new BufferedOutputStream(fos);
+							while ((byteread = is.read(buffer, 0, buffer.length)) != -1) {
+								out.write(buffer, 0, byteread);
+								out.flush();
+							}
+
+							fos.close();
+							out.close();
+
+							break;
+						} catch(Exception e) {
+							System.out.println("Exception: " +e);
+						}
+
+
+					}
+				}
+
+				else {
+					System.out.println(responseLine);
+					if (responseLine.indexOf("~ Bye") != -1)
+						break;
+				}
+			}
+			closed = true;
+		} catch (IOException e) {
+			System.err.println("IOException:  " + e);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 }
